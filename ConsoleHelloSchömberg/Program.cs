407e6cd609eadd22cec43751f38dc2b29f009cb0
egorusegorus <tomaszokraszewski@gmail.com>
2024-07-19 11:19:46 +0200

    Update Program.cs

--- ConsoleHelloSchömberg/Program.cs
+++ ConsoleHelloSchömberg/Program.cs
@@ -5,36 +5,79 @@ namespace ConsoleHelloSchömberg
     internal class Program
     {
         static void Main(string[] args)
-        {//bool exit=false;
-           Lotto();
-            //TaschenRechner(exit);
+        {   //bool exit=false;
+            //Lotto();
+            // TaschenRechner(exit);
             // HelloStadt();
             // summe();
-            
+            //SorrtirenArr();
+            SummeArr();
         }
 
+        public static void SummeArr() { 
+        Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Clear();
+            Console.WriteLine("ConsoleArray2");
+            Console.WriteLine();
+            int[] arWürfe = new int[] { 15, 3, 4, 11, 2, 8 };
+            int a = 0;
+            for (int i = 0; i < arWürfe.Length; i++) {
+                a+=arWürfe[i];
+                if (i == 0) { continue; }
+                else { arWürfe[0] += arWürfe[i]; }
+            }
+            Console.WriteLine("Summe: "+ arWürfe[0]);
+            Console.WriteLine("Summe: " + a);
+            Console.ReadLine();
+        }
+
+        public static void SorrtirenArr() {
+
+            int[] arWürfe = new int[] { 15, 3, 4, 11, 2, 8 };
+            int a = 0;
+            int b = 0;
+
+            for (int i = 0;i<10; i++)
+            {
+                for (int j = 0; j < 5; j++) { 
+                    if (arWürfe[j]> arWürfe[j + 1])
+                {
+                    a = arWürfe[j];
+                    b = arWürfe[j+1];
+                    arWürfe[j]=b;
+                    arWürfe[j+1] = a;
+                }
+                }
+            }
+            Console.WriteLine("Zahlen: " + arWürfe[0] + " " + arWürfe[1] + " " + arWürfe[2] + " " + arWürfe[3] + " " + arWürfe[4] + " " + arWürfe[5]);
+            Console.ReadLine(); 
+        }
+
+
         public static void Lotto()
-        {
+       {
             Random r = new Random();
             int[] ar1 = new int[49];
             int[] ar2 = new int[6];
             for (int i = 0; i < 49; i++)
             {
-                ar1[i] = i+1 ;
+                ar1[i] = i + 1;
             }
 
             for (int i = 0; i < 6; i++)
             {
-                int a =r.Next(1,50);
-                if (a == ar1[a - 1])
+                int a;
+                do
                 {
-                    ar1[a - 1] = 0;
-                    ar2[i]= a;
-                }
-                else { while()}
+                    a = r.Next(1, 50);
+                } while (ar1[a - 1] == 0);
+
+                ar2[i] = a;
+                ar1[a - 1] = 0;
             }
-            Console.WriteLine("Lotto Zahlen: " + ar2[0] + "," + ar2[1] + "," + ar2[2] + "," + ar2[3] + "," + ar2[4] + "," + ar2[5]);
-            // Console.WriteLine("Lotto Zahlen: " + ar2[0] + "," + ar2[1] + "," + ar2[2] + "," + ar2[3] + "," + ar2[4] + "," + ar2[5]);
+
+            Console.WriteLine("Lotto Zahlen: " + ar2[0] + " " + ar2[1] + " " + ar2[2] + " " + ar2[3] + " " + ar2[4] + " " + ar2[5]);
             Console.ReadLine();
         }
 
